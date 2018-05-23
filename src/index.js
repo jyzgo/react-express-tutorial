90e169a5bd8156f579ca7ed317453b66fd07b57c
jyzHome <jyzgo@163.com>
2018-05-23 16:24:26 +1000

    add  bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import  {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+
+ import  '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <BrowserRouter>
