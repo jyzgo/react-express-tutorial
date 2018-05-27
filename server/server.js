c769c7708dad8a159b9ad4eabce14865b3643a43
jyzHome <jyzgo@163.com>
2018-05-27 10:02:34 +1000

    add server config

---
+++ server/server.js
@@ -0,0 +1,16 @@
+//server.js
+
+const express = require('express');
+const app = express();
+const  mongoose = require('mongoose');
+const  bodyParser = require('body-parser');
+const  PORT = 4200;
+const  cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(PORT,function () {
+    console.log('Server is running onPort: ',PORT);
+});
