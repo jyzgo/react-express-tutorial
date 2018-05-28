1444822b63ba7746b63e022f27527daa721844e9
jyzHome <jyzgo@163.com>
2018-05-28 21:33:13 +1000

    add db control

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const  mongoose = require('mongoose');
 const  bodyParser = require('body-parser');
 const  PORT = 4200;
 const  cors = require('cors');
+const  config = require('./database/DB');
+
+mongoose.connect(config.DB).then(
+    ()=>{console.log('Database is connected')},
+    err=>{console.log('Can not connect to the database ' + err)
+    });
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
