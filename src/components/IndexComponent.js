f05b0b2eade796001c4da4277dfd107f13a457c1
jyzHome <jyzgo@163.com>
2018-05-31 23:09:58 +1000

    add constructor  to IndexComponent

--- src/components/IndexComponent.js
+++ src/components/IndexComponent.js
@@ -1,6 +1,16 @@
 import  React, { Component} from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class IndexComponent extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {serverports :[]};
+
+    }
+
+
     render(){
         return (
                 <div style={{marginTop:50}}>
