7f61d46ab5715e99443969a7bcbf3bab0737e7b8
jyzHome <jyzgo@163.com>
2018-06-02 13:56:05 +1000

    display the tablerow

--- src/components/EditComponent.js
+++ src/components/EditComponent.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom';
 
 export default  class EditComponent extends Component{
     render(){
         return(
             <div style={{marginTop:50}}>
             <p> Welcome to Edit Component </p>
+                <Link to ={"/edit/"+this.props.obj._id} className ="btn btn-primary">Edit</Link>
         </div>
         )
     }
